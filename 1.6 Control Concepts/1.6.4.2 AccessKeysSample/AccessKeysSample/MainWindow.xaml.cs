20aa00ba74d4e787557fe9483b4fee0c02932dc3
Rubayat26 <rubayat10me@gmail.com>
2022-08-22 17:28:33 -0400

    Added examples on Control Concepts

---
+++ 1.6 Control Concepts/1.6.4.2 AccessKeysSample/AccessKeysSample/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccessKeysSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button activated: " + (sender as Button).Content.ToString().Replace("_", ""));
+        }
+    }
+}
+
+
+//Here content of the button has "_New/_Open/_Save/_Save _As", so with the Replace() we are replacing
+//the "_" with "" 
