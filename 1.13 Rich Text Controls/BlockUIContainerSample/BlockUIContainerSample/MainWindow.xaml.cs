4b23892b2b820f986d5cdb02411f6489349ee222
Rubayat26 <rubayat10me@gmail.com>
2022-09-02 17:04:30 -0400

    Added examples on RichTextBox and added documentaion for adding a console to WPF application

---
+++ 1.13 Rich Text Controls/BlockUIContainerSample/BlockUIContainerSample/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlockUIContainerSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }        
+    }
+
+
+    public class User                  //This time User is defined in the namespace not
+                                       //in the partial class
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
