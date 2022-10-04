07c5b783fb32a0db550171e47487eb2e16b34685
Rubayat26 <rubayat10me@gmail.com>
2022-10-04 17:23:22 -0400

    Added Examples on DataGrid Control

---
+++ 1.20 DataGrid Control/1.19.1.1 SimpleDataGridSample/SimpleDataGridSample/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleDataGridSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+
+         
+            InitializeComponent();
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            
+        
+            dgSimple.ItemsSource = users;
+
+            
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            var list = dgSimple.ItemsSource as List<User>;
+            txtName.Text = list[0].Name;
+        }
+
+
+
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
